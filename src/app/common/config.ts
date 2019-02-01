61134874a61c1493509d48cf534655d62703b242
Jaskaran Singh <36106441+singhjaskaran2002@users.noreply.github.com>
2019-02-01 12:14:44 +0530

    updation

--- src/app/common/config.ts
+++ src/app/common/config.ts
@@ -1,6 +1,6 @@
 // export const host = 'http://192.168.178.183:8083'; // home wifi Ipv4
 // export const chatHost = 'http://192.168.178.183:8086'; // home wifi
-// export const host = 'http://192.168.1.66:8083'; // office Ipv4
-// export const chatHost = 'http://192.168.1.66:8086'; // office 
-export const host = 'http://localhost:8083'; // office Ipv4
-export const chatHost = 'http://localhost:8086'; // office 
+export const host = 'http://192.168.1.66:8083'; // office Ipv4
+export const chatHost = 'http://192.168.1.66:8086'; // office 
+// export const host = 'http://localhost:8083'; // office Ipv4
+// export const chatHost = 'http://localhost:8086'; // office 
